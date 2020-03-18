989a2a14c4b01fa33b2dfac87e0bf3717f483921
onsuresh <onskcbe@gmail.com>
2020-03-18 21:21:24 +0530

    4 files modified, 2 added

---
+++ src/app/Account/account.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'account-root',
+  templateUrl: 'account.component.html'
+})
+export class AccountComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'
+import { AccountComponent } from './account/account.component'
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ServerComponent,AccountComponent ],
   bootstrap:    [ AppComponent ]  
 })
 export class AppModule { }
